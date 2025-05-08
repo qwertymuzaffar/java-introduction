cd4fdee29c4ebe2063faa8a3cfe5ce89dfc4e3ce
Muzaffar Qosimov <qwertymuzaffar@gmail.com>
2025-05-07 20:16:04 -0400

    Ternary operators

---
+++ src/TernaryOperator.java
@@ -0,0 +1,6 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+        String hasArgs = args.length == 0 ? "No arguments passed" : args[0];
+        System.out.println(hasArgs);
+    }
+}
