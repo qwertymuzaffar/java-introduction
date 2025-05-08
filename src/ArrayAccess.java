b238b5fdb5ed71ee875642d2176d8013cb2cf347
Muzaffar Qosimov <qwertymuzaffar@gmail.com>
2025-05-07 20:34:30 -0400

    Array creation and elements access

---
+++ src/ArrayAccess.java
@@ -0,0 +1,11 @@
+public class ArrayAccess {
+    public static void main(String s[]) {
+        int years[] = {2020,2021,2022,2023,2024,2025};
+        System.out.println(years[0]);
+        System.out.println(years[1]);
+        System.out.println(years[2]);
+        System.out.println(years[3]);
+        System.out.println(years[4]);
+        System.out.println(years[5]);
+    }
+}
