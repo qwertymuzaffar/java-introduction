b05b8a3ce376b66ac9f5575386d30e5212c61926
Muzaffar Qosimov <qwertymuzaffar@gmail.com>
2025-05-07 20:09:23 -0400

    ASCII value of the char

---
+++ src/DataCharToInt.java
@@ -0,0 +1,8 @@
+public class DataCharToInt {
+
+    public static void main(String s[]) {
+        char a = 'a';
+        System.out.println("a is ");
+        System.out.println("ASCII value of a is "+(int)a);
+    }
+}
