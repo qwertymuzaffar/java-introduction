8a205a8fe921355779bd69f00c7ab05bf7df44c1
Muzaffar Qosimov <qwertymuzaffar@gmail.com>
2025-05-07 20:35:40 -0400

    Using 'for' loops to traverse through an array

--- src/ArrayAccess.java
+++ src/ArrayAccess.java
@@ -1,11 +1,10 @@
 public class ArrayAccess {
     public static void main(String s[]) {
         int years[] = {2020,2021,2022,2023,2024,2025};
-        System.out.println(years[0]);
-        System.out.println(years[1]);
-        System.out.println(years[2]);
-        System.out.println(years[3]);
-        System.out.println(years[4]);
-        System.out.println(years[5]);
+        int count_years = years.length;
+        System.out.println("the length of the array is " + count_years);
+        for (int year : years) {
+            System.out.println(year);
+        }
     }
 }
